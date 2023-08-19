dd3c5bdee7442db951cf1f4e430305788da865e8
Kieran O'Leary <128401015+olearykieran@users.noreply.github.com>
2023-08-19 01:02:38 -0400

    Delete amplify directory

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "ytbookmark": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
