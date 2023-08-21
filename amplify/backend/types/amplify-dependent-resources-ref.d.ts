8529303cea8f8018531d813b2534d330f8d94e11
olearykieran <koleary515@gmail.com>
2023-08-21 10:56:56 -0400

    updating backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "ytbookmark": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
